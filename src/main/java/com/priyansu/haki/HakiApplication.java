985d473a68e6c08a7dd932ed7b00a3d3537c591e
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-11 01:07:22 +0530

    Initialization

---
+++ src/main/java/com/priyansu/haki/HakiApplication.java
@@ -0,0 +1,13 @@
+package com.priyansu.haki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakiApplication.class, args);
+	}
+
+}
